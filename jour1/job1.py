5e73f2e67ba50ec7a7a205a128de9c93f1cafbda
warda-mohtaj <115171252+warda-mohtaj@users.noreply.github.com>
2023-03-02 10:43:24 +0100

    Delete jour1 directory

--- jour1/job1.py
+++
@@ -1,7 +0,0 @@
-class Operation:
-    def __init__(self):
-        self.nombre1 = 1
-        self.nombre2 = 2
-
-operation1 = Operation()
-print(operation1)
--- jour1/job2.py
+++
@@ -1,8 +0,0 @@
-class Operation:
-    def __init__(self):
-        self.nombre1 = 1
-        self.nombre2 = 2
-
-operation1 = Operation()
-print(f"Le nombre 1 est {operation1.nombre1}")
-print(f"Le nombre 2 est {operation1.nombre2}")
